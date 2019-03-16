553e5de0a3fb7b0fa3fdf067a60cd64d6bb87ee2
Jwasik <jj.wasik@wp.pl>
2019-03-16 20:40:15 +0100

    initial commit

---
+++ Scharnhorst_server/client.cpp
@@ -0,0 +1,54 @@
+#include "pch.h"
+#include "client.h"
+
+
+Client::Client()
+{
+	this->UdpPort = 0;
+}
+
+
+Client::~Client()
+{
+}
+
+sf::TcpSocket & Client::getTcpSocket()
+{
+	return this->orderSocket;
+}
+
+sf::TcpSocket::Status Client::sendTcp(sf::Packet &packet)
+{
+	return orderSocket.send(packet);
+}
+sf::TcpSocket::Status Client::sendUdp(sf::Packet &packet)
+{
+	if(UdpPort != 0)return orderSocket.send(packet);
+	else return sf::TcpSocket::Status::Error;
+	
+}
+
+sf::TcpSocket::Status Client::receiveTcp(sf::Packet &packet)
+{
+	return orderSocket.receive(packet);
+}
+
+sf::IpAddress Client::getRemoteAddress()
+{
+	return orderSocket.getRemoteAddress();
+}
+
+unsigned int Client::getOutUdpPort()
+{
+	return this->UdpPort;
+}
+
+void Client::setBlocking(bool status)
+{
+	orderSocket.setBlocking(status);
+}
+
+void Client::setOutUdpPort(unsigned int port)
+{
+	this->UdpPort = port;
+}
---
+++ Scharnhorst_server/client.h
@@ -0,0 +1,26 @@
+#pragma once
+#include<SFML/Network.hpp>
+#include<iostream>
+
+class Client
+{
+private:
+	unsigned int UdpPort;
+	sf::TcpSocket orderSocket;
+	sf::UdpSocket outSocket;
+public:
+	Client();
+	~Client();
+	sf::TcpSocket& getTcpSocket();
+	sf::TcpSocket::Status sendTcp( sf::Packet&);
+	sf::TcpSocket::Status sendUdp( sf::Packet&);
+	sf::TcpSocket::Status receiveTcp( sf::Packet&);
+
+	sf::IpAddress getRemoteAddress();
+
+	void setBlocking(bool);
+	void setOutUdpPort(unsigned int);
+	unsigned int getOutUdpPort();
+
+};
+
