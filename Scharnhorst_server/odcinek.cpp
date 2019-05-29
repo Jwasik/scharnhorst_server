fa6f101830d6565c4212de702937608a54b8f39c
Maciej Stefaniak <48411094+madragonse@users.noreply.github.com>
2019-05-29 23:24:51 +0200

    class odcinek
    tak klasa jest tak prosta że nawet ty zrozumiesz

---
+++ Scharnhorst_server/odcinek.cpp
@@ -0,0 +1,121 @@
+#include "odcinek.h"
+
+
+odcinek::odcinek(sf::Vector2f a, sf::Vector2f b) : punkt1(a), punkt2(b)
+{
+	orgin = sf::Vector2f(0, 0);
+
+	line = sf::VertexArray(sf::LineStrip, 2);
+	line[0].position = a;
+	line[1].position = b;
+
+
+
+};
+
+sf::Vector2f punktNaOkregu::zamienNaPunkt(sf::Vector2f orgin)
+{
+	return sf::Vector2f(this->r*sin(sNR(this->a)), -this->r * cos(sNR(this->a))) + orgin;
+
+}
+
+float sNR(float s)
+{
+	return (s / 180.0) * M_PI;
+}
+
+void odcinek::rotate(float angle)
+{
+
+
+	oa = zamienNaPunktNaOkregu(punkt1, orgin);
+	ob = zamienNaPunktNaOkregu(punkt2, orgin);
+
+
+	
+	this->oa.r = this->oa.r + angle;
+	if (this->oa.r > 360)
+	{
+		this->oa.r -= 360;
+	}
+	this->ob.r = this->ob.r + angle;
+	if (this->ob.r > 360)
+	{
+		this->ob.r -= 360;
+	}
+
+
+	punkt1 = oa.zamienNaPunkt(orgin);
+	punkt2 = ob.zamienNaPunkt(orgin);
+
+	
+
+}
+
+
+void odcinek::setOrgin(sf::Vector2f norgin)
+{
+	orgin = norgin;
+}
+
+void odcinek::updateVisual()
+{
+	line[0].position = punkt1;
+	line[1].position = punkt2;
+}
+
+
+void odcinek::prosta(odcinek o, float* A, float* B, float* C)
+{
+	*A = o.punkt1.y - o.punkt2.y; *B = o.punkt2.x - o.punkt1.x; *C = o.punkt1.x*o.punkt2.y - o.punkt2.x*o.punkt1.y;
+}
+
+bool odcinek::isCross(odcinek o1)
+{
+
+
+	float A1, B1, C1, A2, B2, C2;
+	prosta(o1, &A1, &B1, &C1);
+	prosta(*this, &A2, &B2, &C2);
+	return ((((A1*punkt1.x + B1 * punkt1.y + C1)*(A1*punkt2.x + B1 * punkt2.y + C1)) < 0) && (((A2*o1.punkt1.x + B2 * o1.punkt1.y + C2)*(A2*o1.punkt2.x + B2 * o1.punkt2.y + C2)) < 0));
+
+}
+
+Hitbox::odcinek::odcinek()
+{
+
+}
+
+punktNaOkregu odcinek::zamienNaPunktNaOkregu(sf::Vector2f punkt, sf::Vector2f srodekOkregu)
+{
+	punktNaOkregu tem;
+	tem.r = sqrt(pow(punkt.x, 2) + pow(punkt.y, 2));//d³ugoœæ tego promienia
+
+	tem.a = (atan(punkt.y / punkt.x) / M_PI * 180);//k¹t pomiêdzi pionowym promieniem a promieniem do punktu temx, temy
+
+	if (punkt.x >= 0 && punkt.y < 0)//ustala ¿e k¹t 0 stopni jest skierowany w górê 
+	{
+		tem.a += 90;
+	}
+	else if (punkt.x > 0 && punkt.y >= 0)
+	{
+		tem.a += 90;
+	}
+	else if (punkt.x <= 0 && punkt.y > 0)
+	{
+		tem.a += 270;
+	}
+	else
+	{
+		tem.a += 270;
+	}
+	return tem;
+}
+
+void odcinek::move(sf::Vector2f moveBy)
+{
+	punkt1 += moveBy;
+	punkt2 += moveBy;
+	orgin += moveBy;
+
+}
---
+++ Scharnhorst_server/odcinek.h
@@ -0,0 +1,40 @@
+#include "pch.h"
+#include <SFML/Graphics.hpp>
+#include <iostream>
+#include <cmath> //pierwiastkie i potêgi
+#include <windows.h>
+
+#pragma once
+
+
+static float sNR(float s);//stopnie na radiany
+
+struct punktNaOkregu // punkt zorientowany w sposób odleg³oœæ od œrodka okrêgu: 'r', i k¹t 'a' pomiêdzy pionowym w górê promieniem 
+{
+	sf::Vector2f zamienNaPunkt(sf::Vector2f orgin);
+	float r;
+	float a;
+};
+
+class odcinek
+{
+	sf::Vector2f orgin;//zmieniaj aby przemieœciæ przy rotate() siê aktualizuje
+
+	punktNaOkregu oa, ob; //i tak musz¹ byæ u¿ywane przy ka¿dej rotacji wiêc niech s¹ dostêpne tu ca³y czas 
+	sf::Vector2f punkt1, punkt2; //punkty
+	sf::VertexArray line; //wizualizacja
+
+	odcinek(sf::Vector2f punkt1, sf::Vector2f punkt2);
+	static punktNaOkregu zamienNaPunktNaOkregu(sf::Vector2f punkt, sf::Vector2f srodekOkregu);
+	void prosta(odcinek o, float* A, float* B, float* C); //liczy parametry równania prostej na podstawie 'odcinka'
+	bool isCross(odcinek o1); //sprawdza czy odcinki siê przecinaj¹
+
+	void rotate(float a); //obraca woko³o orgin bo woko³o czegoœ musi
+	void move(sf::Vector2f moveBy);//przemieszcza punkty i orgin
+	void updateVisual(); //aktualizuje line
+	void setOrgin(sf::Vector2f);//ustawia orgin
+
+	odcinek();
+
+
+};
